86e75cc65d49ad6886315a2b3442b6f7c20305b2
ravsodhi <ravsimar@gmail.com>
2023-11-24 18:57:32 +0530

    add config command group

---
+++ src/commands/config.ts
@@ -0,0 +1,47 @@
+import { SlashCommandBuilder, SlashCommandSubcommandBuilder } from "discord.js";
+
+import { CommandHandler, Command } from "../models";
+import { errorHandler } from "../utils/errorHandler";
+
+import { handleSetReaderRole } from "./subcommands/config";
+
+const handlers: Record<string, CommandHandler> = {
+  setreaderrole: handleSetReaderRole,
+};
+
+const configSetReaderRoleSubcommand = new SlashCommandSubcommandBuilder()
+  .setName("setreaderrole")
+  .setDescription("Sets a role as a reader role")
+  .addRoleOption((option) =>
+    option.setName("role").setDescription("The role").setRequired(true),
+  )
+  .addIntegerOption((option) =>
+    option
+      .setName("points")
+      .setDescription("The minimum required points for the role")
+      .setRequired(true),
+  );
+
+export const config: Command = {
+  data: new SlashCommandBuilder()
+    .setName("config")
+    .setDescription("For guild config management")
+    .addSubcommand(configSetReaderRoleSubcommand)
+    .setDMPermission(false),
+  run: async (bot, interaction, guildConfig) => {
+    try {
+      const subCommand = interaction.options.getSubcommand();
+      const handler = handlers[subCommand];
+      await handler(bot, interaction, guildConfig);
+    } catch (err) {
+      await errorHandler(
+        bot,
+        "commands > config",
+        err,
+        interaction.guild?.name,
+        undefined,
+        interaction,
+      );
+    }
+  },
+};
